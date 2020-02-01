d10456fe2e7659a67f2fca29f3c8988fb9f904d2
so0 <syngshm@gmail.com>
2020-02-01 17:50:53 +0900

    props

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import MyComponent from './MyComponent';
 const App = () => {
-  return <MyComponent />;
+  // return <MyComponent name="React">리액트</MyComponent>;
+  return <MyComponent>리액트</MyComponent>;
 };
 
 export default App;
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const MyComponent = () => {
-  return <div>나의 새롭고 멋진 컴포넌트</div>;
+const MyComponent = props => {
+  const { name, children } = props;
+  return (
+    <div>
+      안녕하세요 제 이름은 {name} 입니다.
+      <br />
+      children 값은 {children} 입니다.
+    </div>
+  );
 };
 
+MyComponent.defaultProps = {
+  name: '기본 이름'
+};
 export default MyComponent;
