9c19dddc8a11bceecb92b72990c9304f160eade5
so0 <syngshm@gmail.com>
2020-02-01 19:52:36 +0900

    event

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-// import MyComponent from './MyComponent';
-// import Counter from './Counter';
-import Say from './Say';
+import EventPractice from './EventPractice';
 const App = () => {
-  return <Say />;
+  return <EventPractice />;
 };
 
 export default App;
