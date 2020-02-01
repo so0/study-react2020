dbf0aa40db8ad009b1dbc4a25147e565c4f12e22
so0 <syngshm@gmail.com>
2020-02-01 17:46:09 +0900

    클래스형 컴포넌트

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,32 +1,11 @@
-import React, { Fragment } from 'react';
-import './App.css';
-function App() {
-  const name = '리액트';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '48px',
-    fontWeight: 'bold',
-    padding: 16
-  };
-  return (
-    <Fragment>
-      <h1> 리액트 안녕</h1>
-      <h2>잘 작동하니?</h2>
-      {/* 조건부 렌더링 */}
-      <div>
-        {name === '리액트' ? (
-          <h1>리액트입니다.</h1>
-        ) : (
-          <h1>리액트가 아닙니다.</h1>
-        )}
-      </div>
+import React, { Component } from 'react';
 
-      <div> {name === '리액트' ? <h1>리액트</h1> : null}</div>
-      <div style={style}>{name}</div>
-      <div className="react">{name}</div>
-    </Fragment>
-  );
+// 클래스형 컴포넌트
+class App extends Component {
+  render() {
+    const name = 'react';
+    return <div className="react">{name}</div>;
+  }
 }
 
 export default App;
