33cb69fcd237c005791e2e47e8fdf9030cffbe34
so0 <syngshm@gmail.com>
2020-02-01 17:30:47 +0900

    chapter2 jsx

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
-
 function App() {
+  const name = '리액트';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1> 리액트 안녕</h1>
+      <h2>잘 작동하니?</h2>
+      {/* 조건부 렌더링 */}
+      <div>
+        {name === '리액트' ? (
+          <h1>리액트입니다.</h1>
+        ) : (
+          <h1>리액트가 아닙니다.</h1>
+        )}
+      </div>
+
+      <div> {name === '리액트' ? <h1>리액트</h1> : null}</div>
+      <div style={style}>{name}</div>
+      <div className="react">{name}</div>
+    </Fragment>
   );
 }
 
