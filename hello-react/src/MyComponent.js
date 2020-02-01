bfb19a2a6793af4ea27696a773600dbb0eaaac3c
so0 <syngshm@gmail.com>
2020-02-01 17:57:44 +0900

    클래스형 컴포넌트 props

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'props-types';
-const MyComponent = props => {
-  const { name, children } = props;
-  return (
-    <div>
-      안녕하세요 제 이름은 {name} 입니다.
-      <br />
-      children 값은 {children} 입니다.
-      <br />
-      제가 좋아하는 숫자는 {favoriteNumber} 입니다.
-    </div>
-  );
-};
+class MyComponent extends Component {
+  static defaultProps = {
+    name: '기본 이름'
+  };
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired
+  };
+  render() {
+    const { name, children } = this.props;
+
+    return (
+      <div>
+        안녕하세요 제 이름은 {name} 입니다.
+        <br />
+        children 값은 {children} 입니다.
+        <br />
+        제가 좋아하는 숫자는 {favoriteNumber} 입니다.
+      </div>
+    );
+  }
+}
 
 MyComponent.defaultProps = {
   name: '기본 이름'
