4d00b69f04f694e037cc1c5c6064d6bf04148a5a
so0 <syngshm@gmail.com>
2020-02-01 19:55:41 +0900

    input 여러개 처리

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -2,53 +2,40 @@ import React, { Component } from 'react';
 
 class EventPractice extends Component {
   state = {
-    message: ''
+    message: '',
+    username: ''
   };
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleChange(e) {
+  handleChange = e => {
     this.setState({
-      message: e.target.value
+      [e.target.name]: e.target.value
     });
-  }
-  handleClick() {
-    alert(this.state.message);
+  };
+  handleClick = () => {
+    alert(this.state.username + ' : ' + this.state.message);
     this.setState({
-      message: ''
+      message: '',
+      username: ''
     });
-  }
+  };
   render() {
     return (
       <div>
         <h1>이벤트 연습</h1>
         <input
           type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
-          //   onChange={e => {
-          //     console.log(e);
-          //     console.log(e.target.value);
-          //     this.setState({
-          //       message: e.target.value
-          //     });
-          //   }}
           onChange={this.handleChange}
         />
-        <button
-          //   onClick={() => {
-          //     alert(this.state.message);
-          //     this.setState({
-          //       message: ''
-          //     });
-          //   }}
-          onClick={this.handleClick}
-        >
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
