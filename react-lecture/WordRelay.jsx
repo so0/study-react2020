be5fbd5c00b7417fafbcc030561f6b12905614d1
So0 <syngshm@gmail.com>
2020-02-23 22:31:49 +0900

    웹팩으로 빌드하기

--- react-lecture/WordRelay.jsx
+++ react-lecture/WordRelay.jsx
@@ -1,9 +1,13 @@
 const React = require('react');
 const { Component } = React;
 
-class WordRelay extends Componnet {
-  state = {};
-  render() {}
+class WordRelay extends Component {
+  state = {
+    text: 'Hello, webpack'
+  };
+  render() {
+    return <h1>{this.state.text}</h1>;
+  }
 }
 
 module.exports = WordRelay;
--- react-lecture/webpack.config.js
+++ react-lecture/webpack.config.js
@@ -9,6 +9,18 @@ module.exports = {
   entry: {
     app: ['./client']
   }, // 입력
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/, // js, jsx
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/plugin-proposal-class-properties']
+        }
+      }
+    ]
+  },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.js'
