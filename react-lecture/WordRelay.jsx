38c0a499e884cc4c111b1b31de5fc0e2b3f84533
So0 <syngshm@gmail.com>
2020-02-29 17:51:33 +0900

    webpack-dev-server ,hot-loader

--- react-lecture/WordRelay.jsx
+++ react-lecture/WordRelay.jsx
@@ -37,7 +37,7 @@ class WordRelay extends Component {
         <div>{this.state.word}</div>
         <form onSubmit={this.onSubmitForm}>
           <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
-          <button>입력</button>
+          <button>클릭 !!</button>
         </form>
         <div>{this.state.result}</div>
       </>
--- react-lecture/webpack.config.js
+++ react-lecture/webpack.config.js
@@ -27,13 +27,14 @@ module.exports = {
             ],
             '@babel/preset-react'
           ],
-          plugins: ['@babel/plugin-proposal-class-properties']
+          plugins: ['@babel/plugin-proposal-class-properties', 'react-hot-loader/babel']
         }
       }
     ]
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'app.js'
+    filename: 'app.js',
+    publicPath: '/dist/'
   } // 출력
 };
