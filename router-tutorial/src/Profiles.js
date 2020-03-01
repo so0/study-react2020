80248546313d97c6a79ad98508507c17233ff2e5
So0 <syngshm@gmail.com>
2020-03-01 23:11:03 +0900

    profile에 withRouter 사용

--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import Profile from './Profile';
-import WithRouterSample from './WithRouterSample';
+// import WithRouterSample from './WithRouterSample';
 
 const Profiles = () => {
   return (
@@ -19,7 +19,7 @@ const Profiles = () => {
       <Route path="/profiles" exact render={() => <div>사용자를 선택해주세요.</div>} />
       <Route path="/profiles/:username" component={Profile} />
 
-      <WithRouterSample />
+      {/* <WithRouterSample /> */}
     </div>
   );
 };
