e9f527590107c4342262deef7bbfd1a7567958fb
So0 <syngshm@gmail.com>
2020-03-01 22:49:25 +0900

    URL 파라미터

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Profile from './Profile';
 
 function App() {
   return (
@@ -13,12 +14,19 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       {/* <Route path="/about" component={About} /> */}
       {/* <Route path="/info" component={About} /> */}
       <Route path={['/about', '/info']} component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 }
---
+++ router-tutorial/src/Profile.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const data = {
+  velopert: {
+    name: '김민준',
+    description: '리액트를 좋아하는 개발자'
+  },
+  gildong: {
+    name: '홍길동',
+    description: '고전 소설 홍길동전의 주인공'
+  }
+};
+
+// URL 파라미터 사용 시 match 라는 개체의 params 값 참조
+const Profile = ({ match }) => {
+  console.log('match', match);
+  const { username } = match.params;
+  const profile = data[username];
+  if (!profile) {
+    return <div>존재하지 않는 사용자입니다.</div>;
+  }
+  return (
+    <div>
+      <h3>
+        {username}({profile.name})
+      </h3>
+      <p>{profile.description}</p>
+    </div>
+  );
+};
+
+export default Profile;
