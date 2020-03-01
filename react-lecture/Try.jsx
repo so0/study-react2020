f5e051cf6233024401c58e5372ce069a7982a728
So0 <syngshm@gmail.com>
2020-03-01 18:16:42 +0900

    props와 state연결하기

--- react-lecture/Try.jsx
+++ react-lecture/Try.jsx
@@ -1,6 +1,15 @@
-import React, { PureComponent, memo } from 'react';
+import React, { PureComponent, memo, useState } from 'react';
 
 // class Try extends PureComponent {
+//   // 부모로 부터 받은 props 는 변경하면 안됨.  props로 state 로 만들어서 state를 바꾸어야함.
+//   constructor(props) {
+//     super(props);
+//     const filtered = this.props.filter(() => {});
+//     this.state = {
+//       result: filtered,
+//       try: this.props.try
+//     };
+//   }
 //   render() {
 //     const { tryInfo } = this.props;
 //     return (
@@ -14,6 +23,12 @@ import React, { PureComponent, memo } from 'react';
 
 // React.memo 적용
 const Try = memo(({ tryInfo }) => {
+  // 부모로 부터 받은 props 는 변경하면 안됨.  props로 state 로 만들어서 state를 바꾸어야함.
+  const [result, setResult] = useState(tryInfo.result);
+  const onClick = () => {
+    setResult('1');
+  };
+
   return (
     <li>
       <div>{tryInfo.try}</div>
