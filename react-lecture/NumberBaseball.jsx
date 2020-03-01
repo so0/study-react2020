1b27ebd90d31085103f3908416ad42ea5047bba1
So0 <syngshm@gmail.com>
2020-03-01 17:17:37 +0900

    숫자야구 hook으로 전환

--- react-lecture/NumberBaseball.jsx
+++ react-lecture/NumberBaseball.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Try from './Try';
 
 function getNumbers() {
@@ -11,84 +11,71 @@ function getNumbers() {
   }
   return array;
 }
-class NumberBaseball extends Component {
-  state = {
-    result: '',
-    value: '',
-    answer: getNumbers(), //ex [1,3,5,7]
-    tries: []
+const NumberBaseball = () => {
+  const [result, setResult] = useState('');
+  const [value, setValue] = useState('');
+  const [answer, setAnswer] = useState(getNumbers());
+  const [tries, setTries] = useState([]);
+
+  console.log('tries', tries);
+  const onChangeInput = e => {
+    setValue(e.target.value);
   };
 
-  onChangeInput = e => {
-    this.setState({
-      value: e.target.value
-    });
-  };
-  onSubmitForm = e => {
-    console.log('onSubmitForm');
+  const onSubmitForm = e => {
     e.preventDefault();
-    if (this.state.value === this.state.answer.join('')) {
-      this.setState(prevState => {
-        return {
-          results: '홈런!',
-          tries: [...prevState.tries, { try: prevState.value, result: '홈런!' }]
-        };
+    if (value === answer.join('')) {
+      setResult('홈런');
+      setTries(prevTries => {
+        return [...prevTries, { try: value, result: '홈런!' }];
       });
       alert('게임을 다시 시작합니다!');
-      this.setState({
-        value: '',
-        answer: getNumbers(),
-        tries: []
-      });
+      setValue('');
+      setAnswer(getNumbers());
+      setTries([]);
     } else {
       // 틀렸을 때
-      const answerArray = this.state.value.split('').map(v => parseInt(v));
+      console.log('tries', tries);
+      const answerArray = value.split('').map(v => parseInt(v));
       let strike = 0;
       let ball = 0;
-      if (this.state.tries.length >= 9) {
-        this.setState({
-          result: `10번 넘게 틀려서 실패 답은 ${this.state.answer.join(',')} 입니다.`
-        });
+      if (tries.length >= 9) {
+        setResult(`10번 넘게 틀려서 실패 답은 ${answer.join(',')} 입니다.`);
+
         alert('게임을 다시 시작합니다!');
-        this.setState({
-          value: '',
-          answer: getNumbers(),
-          tries: []
-        });
+        setValue('');
+        setAnswer(getNumbers());
+        setTries([]);
       } else {
         for (let i = 0; i < 4; i++) {
-          if (answerArray[i] === this.state.answer[i]) {
+          if (answerArray[i] === answer[i]) {
             strike += 1;
-          } else if (this.state.answer.includes(answerArray[i])) {
+          } else if (answer.includes(answerArray[i])) {
             ball += 1;
           }
         }
-        this.setState(prevState => {
-          return {
-            tries: [...prevState.tries, { try: prevState.value, result: `${strike} 스트라이크, ${ball} 볼입니다.` }]
-          };
+        setTries(prevTries => {
+          return [...prevTries, { try: value, result: `${strike} 스트라이크, ${ball} 볼입니다.` }];
         });
       }
     }
   };
-  render() {
-    console.log(this.state.fruits);
-    return (
-      <div>
-        <h1>{this.state.result}</h1>
-        <form onSubmit={this.onSubmitForm}>
-          <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}></input>
-        </form>
-        <div>시도: {this.state.tries.length}</div>
-        <ul>
-          {this.state.tries.map((v, i) => {
-            return <Try key={`${i + 1}차 시도 : `} tryInfo={v} />;
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
+
+  return (
+    <div>
+      <h1>{result}</h1>
+      <form onSubmit={onSubmitForm}>
+        <input maxLength={4} value={value} onChange={onChangeInput}></input>
+      </form>
+      <div>시도: {tries.length}</div>
+      <ul>
+        {tries.map((v, i) => {
+          return <Try key={`${i + 1}차 시도 : `} tryInfo={v} />;
+        })}
+      </ul>
+    </div>
+  );
+};
 
 // export const hello = 'hello'; // import { hello } 이런식으로 가져옴, 여러 번 쓸 수 있다.
 // export const bye = 'bye'; // import { hello, bye }
