3ff99afcd8bc03b3f7b9deced1e5f83ba97a1e73
So0 <syngshm@gmail.com>
2020-03-01 22:53:37 +0900

    쿼리

--- router-tutorial/src/About.js
+++ router-tutorial/src/About.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import qs from 'qs';
 
-const About = () => {
+const About = ({ location }) => {
+  console.log('location', location);
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true // 문자열 맨 앞의 ? 생략
+  });
+  console.log('query', query);
+  const showDetail = query.detail === 'true';
   return (
     <div>
       <h1>소개</h1>
       <p>이 프로젝트는 리액트 라우터 기초를 실습해보는 예제 프로젝트입니다.</p>
+      {showDetail && <p>detail 값을 true 로 설정하셨군요</p>}
     </div>
   );
 };
