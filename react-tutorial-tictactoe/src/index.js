0e3733f245a9f5ca6111519bbca3cb5625a207b0
So0 <syngshm@gmail.com>
2020-03-30 23:57:54 +0900

    Square component state  추가

--- react-tutorial-tictactoe/src/index.js
+++ react-tutorial-tictactoe/src/index.js
@@ -1,6 +1,20 @@
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          this.setState({ value: 'X' });
+        }}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
