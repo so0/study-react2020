819d5df058fc76cb75d7f592bd292359ca3446f6
So0 <syngshm@gmail.com>
2020-03-31 00:09:35 +0900

    X, O 순서 만들기

--- react-tutorial-tictactoe/src/index.js
+++ react-tutorial-tictactoe/src/index.js
@@ -14,7 +14,8 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
   renderSquare(i) {
@@ -27,12 +28,12 @@ class Board extends React.Component {
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
