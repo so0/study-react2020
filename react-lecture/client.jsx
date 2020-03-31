7f141be7e59ebf1e73b91cedc55c24aca0ee5261
So0 <syngshm@gmail.com>
2020-03-31 23:49:03 +0900

    반응속도체크 컴포넌트

--- react-lecture/client.jsx
+++ react-lecture/client.jsx
@@ -5,10 +5,12 @@ import { hot } from 'react-hot-loader/root';
 // const ReactDOM = require('react-dom');
 // const { hot } = require('react-hot-loader/root');
 
+import ResponseCheck from './ResponseCheck';
 import WordRelay from './WordRelay';
 import NumberBaseball from './NumberBaseball';
 import RenderTest from './RenderTest';
 // const WordRelay = require('./WordRelay');
-const Hot = hot(NumberBaseball);
+const Hot = hot(ResponseCheck);
+// const Hot = hot(NumberBaseball);
 // const Hot = hot(RenderTest); // PureComponent
 ReactDOM.render(<Hot />, document.querySelector('#root'));
