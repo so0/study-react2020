3faa4518abdb3895898c921d82afef48497f652d
So0 <syngshm@gmail.com>
2020-04-03 20:33:24 +0900

    틱택토 컴포넌트 생성

---
+++ react-lecture/TicTacToe.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useReducer } from 'react';
+import Table from './Table';
+
+import React from 'react';
+
+const initialState = {
+  winner: '',
+  turn: 'O',
+  tableData = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ]
+}
+
+const reducer = (state, action) => {
+  
+}
+
+const TicTacToe = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // const [winner, setWinner] = useState('');
+  // const [turn, setTurn] = useState('O');
+  // const [tableData, setTableData] = useState([
+  //   ['', '', ''],
+  //   ['', '', ''],
+  //   ['', '', ''],
+  // ]);
+
+  return (
+    <>
+      <Table />
+      {winner && <div>{winner}님의 승리</div>}
+    </>
+  );
+};
+
+export default TicTacToe;
