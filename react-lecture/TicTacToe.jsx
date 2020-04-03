dafa8e66051a209fc78f11ec18a836115d300286
So0 <syngshm@gmail.com>
2020-04-03 20:52:44 +0900

    컴포넌트 모양 완성

--- react-lecture/TicTacToe.jsx
+++ react-lecture/TicTacToe.jsx
@@ -1,21 +1,28 @@
-import React, { useState, useReducer } from 'react';
+import React, { useReducer, useCallback } from 'react';
 import Table from './Table';
-
-import React from 'react';
 
 const initialState = {
   winner: '',
   turn: 'O',
-  tableData = [
+  tableData: [
     ['', '', ''],
     ['', '', ''],
     ['', '', ''],
-  ]
-}
+  ],
+};
+
+const SET_WINNER = 'SET_WINNER';
 
 const reducer = (state, action) => {
-  
-}
+  switch (action.type) {
+    case SET_WINNER:
+      return {
+        ...state,
+        winner: action.winner,
+      };
+      break;
+  }
+};
 
 const TicTacToe = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -27,10 +34,14 @@ const TicTacToe = () => {
   //   ['', '', ''],
   // ]);
 
+  const onClickTable = useCallback(() => {
+    // dispatch(action)
+    dispatch({ type: 'SET_WINNER', winner: 'O' });
+  }, []);
   return (
     <>
-      <Table />
-      {winner && <div>{winner}님의 승리</div>}
+      <Table onClick={onClickTable} tableData={state.tableData} />
+      {state.winner && <div>{state.winner}님의 승리</div>}
     </>
   );
 };
--- react-lecture/client.jsx
+++ react-lecture/client.jsx
@@ -5,13 +5,15 @@ import { hot } from 'react-hot-loader/root';
 // const ReactDOM = require('react-dom');
 // const { hot } = require('react-hot-loader/root');
 
+import TicTacToe from './TicTacToe';
 import Lotto from './Lotto';
 import RSP from './RSP';
 import ResponseCheck from './ResponseCheck';
 import WordRelay from './WordRelay';
 import NumberBaseball from './NumberBaseball';
 import RenderTest from './RenderTest';
-const Hot = hot(Lotto);
+const Hot = hot(TicTacToe);
+// const Hot = hot(Lotto);
 // const WordRelay = require('./WordRelay');
 // const Hot = hot(ResponseCheck);
 // const Hot = hot(NumberBaseball);
