a118d9710e488165f4b8f433e60a9522fe217867
So0 <syngshm@gmail.com>
2020-04-03 21:58:24 +0900

    틱택토 구현하기

--- react-lecture/Td.jsx
+++ react-lecture/Td.jsx
@@ -1,11 +1,16 @@
 import React, { useCallback } from 'react';
-import { CLICK_CELL, CHANGE_TURN } from './TicTacToe';
+import { CLICK_CELL } from './TicTacToe';
 const Td = ({ rowIndex, cellIndex, cellData, dispatch }) => {
   const onClickTd = useCallback(() => {
     console.log(rowIndex, cellIndex);
+    if (cellData) {
+      // 이미 클릭한 곳 클릭 못하게함
+      return;
+    }
+    // state가 비동기적으로 변경됨
     dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex });
-    dispatch({ type: CHANGE_TURN });
-  }, []);
+    // dispatch({ type: CHANGE_TURN });
+  }, [cellData]);
   return <td onClick={onClickTd}>{cellData}</td>;
 };
 
--- react-lecture/TicTacToe.jsx
+++ react-lecture/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useCallback } from 'react';
+import React, { useReducer, useCallback, useEffect } from 'react';
 import Table from './Table';
 
 const initialState = {
@@ -9,11 +9,13 @@ const initialState = {
     ['', '', ''],
     ['', '', ''],
   ],
+  recentCell: [-1, -1],
 };
 
 export const SET_WINNER = 'SET_WINNER';
 export const CLICK_CELL = 'CLICK_CELL';
 export const CHANGE_TURN = 'CHANGE_TURN';
+export const RESET_GAME = 'RESET_GAME';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -23,24 +25,44 @@ const reducer = (state, action) => {
         winner: action.winner,
       };
     case CLICK_CELL:
+      console.log('CLICK_CELL');
       const tableData = [...state.tableData];
       tableData[action.row] = [...tableData[action.row]];
       tableData[action.row][action.cell] = state.turn;
+      console.log('action', action);
       return {
         ...state,
         tableData,
+        recentCell: [action.row, action.cell],
       };
     case CHANGE_TURN: {
+      console.log('CHANGE_TURN');
       return {
         ...state,
         turn: state.turn === 'O' ? 'X' : 'O',
       };
     }
+    case RESET_GAME: {
+      console.log('RESET_GAME');
+      return {
+        ...state,
+        turn: 'O',
+        tableData: [
+          ['', '', ''],
+          ['', '', ''],
+          ['', '', ''],
+        ],
+        recentCell: [-1, -1],
+      };
+    }
+    default:
+      return state;
   }
 };
 
 const TicTacToe = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { tableData, turn, winner, recentCell } = state;
   // const [winner, setWinner] = useState('');
   // const [turn, setTurn] = useState('O');
   // const [tableData, setTableData] = useState([
@@ -53,6 +75,66 @@ const TicTacToe = () => {
     // dispatch(action)
     dispatch({ type: 'SET_WINNER', winner: 'O' });
   }, []);
+
+  useEffect(() => {
+    const [row, cell] = recentCell;
+    console.log('useEffect', row, cell, turn);
+    if (row < 0) {
+      return;
+    }
+    let win = false;
+    if (
+      tableData[row][0] === turn &&
+      tableData[row][1] === turn &&
+      tableData[row][2] === turn
+    ) {
+      win = true;
+    }
+    if (
+      tableData[0][cell] === turn &&
+      tableData[1][cell] === turn &&
+      tableData[2][cell] === turn
+    ) {
+      win = true;
+    }
+    if (
+      row === cell &&
+      tableData[0][0] === turn &&
+      tableData[1][1] === turn &&
+      tableData[2][2] === turn
+    ) {
+      win = true;
+    }
+    if (
+      tableData[0][2] === turn &&
+      tableData[1][1] === turn &&
+      tableData[2][0] === turn
+    ) {
+      win = true;
+    }
+
+    if (win) {
+      // 승리
+      dispatch({ type: SET_WINNER, winner: turn });
+      dispatch({ type: RESET_GAME });
+    } else {
+      let all = true;
+      tableData.forEach(row => {
+        row.forEach(cell => {
+          if (!cell) {
+            all = false;
+          }
+        });
+      });
+
+      if (all) {
+        // 무승부
+        dispatch({ type: RESET_GAME });
+      } else {
+        dispatch({ type: CHANGE_TURN });
+      }
+    }
+  }, [recentCell]);
   return (
     <>
       <Table
