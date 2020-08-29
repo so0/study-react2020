03ba8b4c7890e64bc755a25a34c208197e2f348f
so0 <syngshm@gmail.com>
2020-08-29 23:05:14 +0900

    useReducer로 변경

--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,32 +1,64 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case 'SUCCESS':
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case 'ERROR':
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, {
+    data: null,
+    loading: false,
+    error: null,
+  });
+  // const [users, setUsers] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
 
   const fetchUsers = async () => {
+    dispatch({ type: 'LOADING' });
+    // setLoading(true);
     try {
       // 요청이 시작 할 때에는 error 와 users 를 초기화하고
-      setError(null);
-      setUsers(null);
+      // setUsers(null);
       // loading 상태를 true 로 바꿉니다.
-      setLoading(true);
       const response = await axios.get(
         'https://jsonplaceholder.typicode.com/users'
       );
-      setUsers(response.data); // 데이터는 response.data 안에 들어있습니다.
+      // setUsers(response.data); // 데이터는 response.data 안에 들어있습니다.
+      dispatch({ type: 'SUCCESS', data: response.data });
     } catch (e) {
-      setError(e);
+      dispatch({ type: 'ERROR', error: e });
+      // setError(e);
     }
-    setLoading(false);
+    // setLoading(false);
   };
 
   useEffect(() => {
     fetchUsers();
   }, []);
-
+  const { loading, error, data: users } = state;
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!users) return null;
