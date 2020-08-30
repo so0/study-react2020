520f215bd5b179e10f652add0b774696273a7664
so0 <syngshm@gmail.com>
2020-08-30 19:12:44 +0900

    라우터 파라미터, 쿼리

--- router-tutorial2/src/About.js
+++ router-tutorial2/src/About.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import qs from 'qs';
 
-const About = () => {
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+  console.log('query', query);
+  const detail = query.detail === 'true';
   return (
     <div>
       <h1>소개</h1>
       <p>이 프로젝트는 리액트 라우터 기초를 실습해보는 예제 프로젝트랍니다.</p>
+      {detail && <p>추가적인 정보가 어쩌고 저쩌고..</p>}
     </div>
   );
 };
--- router-tutorial2/src/App.js
+++ router-tutorial2/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Profile from './Profile';
 
 function App() {
   return (
@@ -15,10 +16,14 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile">소개</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/profiles/:username" component={Profile} />
     </div>
   );
 }
