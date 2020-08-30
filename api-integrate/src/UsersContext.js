cccf4da0963ffcf0c31110b0f969259f2a5633a3
so0 <syngshm@gmail.com>
2020-08-30 18:29:18 +0900

    api.js 분리, 반복 코드 제거

--- api-integrate/src/UsersContext.js
+++ api-integrate/src/UsersContext.js
@@ -1,6 +1,7 @@
 import React, { createContext, useReducer, useContext } from 'react';
 import axios from 'axios';
-
+import createAsyncDispatcher from './asyncActionUtils';
+import * as api from './api'; // api 파일에서 내보낸 모든 함수들을 불러옴
 // UsersContext 에서 사용 할 기본 상태
 const initialState = {
   users: {
@@ -108,26 +109,6 @@ export function useUsersDispatch() {
   return dispatch;
 }
 
-export async function getUsers(dispatch) {
-  dispatch({ type: 'GET_USERS' });
-  try {
-    const response = await axios.get(
-      'https://jsonplaceholder.typicode.com/users'
-    );
-    dispatch({ type: 'GET_USERS_SUCCESS', data: response.data });
-  } catch (e) {
-    dispatch({ type: 'GET_USERS_ERROR', error: e });
-  }
-}
+export const getUsers = createAsyncDispatcher('GET_USERS', api.getUsers);
 
-export async function getUser(dispatch, id) {
-  dispatch({ type: 'GET_USER' });
-  try {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/users/${id}`
-    );
-    dispatch({ type: 'GET_USER_SUCCESS', data: response.data });
-  } catch (e) {
-    dispatch({ type: 'GET_USER_ERROR', error: e });
-  }
-}
+export const getUser = createAsyncDispatcher('GET_USER', api.getUser);
