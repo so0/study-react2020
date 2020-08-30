e6769aacb3b05b939d17b3d518e7afb4fd4a7870
so0 <syngshm@gmail.com>
2020-08-30 18:38:44 +0900

    리듀서 함수화

--- api-integrate/src/UsersContext.js
+++ api-integrate/src/UsersContext.js
@@ -1,75 +1,51 @@
 import React, { createContext, useReducer, useContext } from 'react';
 import axios from 'axios';
-import createAsyncDispatcher from './asyncActionUtils';
+import {
+  createAsyncDispatcher,
+  createAsyncHandler,
+  initialAsyncState,
+} from './asyncActionUtils';
 import * as api from './api'; // api 파일에서 내보낸 모든 함수들을 불러옴
 // UsersContext 에서 사용 할 기본 상태
 const initialState = {
-  users: {
-    loading: false,
-    data: null,
-    error: null,
-  },
-  user: {
-    loading: false,
-    data: null,
-    error: null,
-  },
+  users: initialAsyncState,
+  user: initialAsyncState,
 };
 
-// 로딩중일 때 바뀔 상태 객체
-const loadingState = {
-  loading: true,
-  data: null,
-  error: null,
-};
+// // 로딩중일 때 바뀔 상태 객체
+// const loadingState = {
+//   loading: true,
+//   data: null,
+//   error: null,
+// };
 
-// 성공했을 때의 상태 만들어주는 함수
-const success = (data) => ({
-  loading: false,
-  data,
-  error: null,
-});
+// // 성공했을 때의 상태 만들어주는 함수
+// const success = (data) => ({
+//   loading: false,
+//   data,
+//   error: null,
+// });
 
-// 실패했을 때의 상태 만들어주는 함수
-const error = (error) => ({
-  loading: false,
-  data: null,
-  error: error,
-});
+// // 실패했을 때의 상태 만들어주는 함수
+// const error = (error) => ({
+//   loading: false,
+//   data: null,
+//   error: error,
+// });
 
+const usersHandler = createAsyncHandler('GET_USERS', 'users');
+const userHandler = createAsyncHandler('GET_USER', 'user');
 // 위에서 만든 객체 / 유틸 함수들을 사용하여 리듀서 작성
 function usersReducer(state, action) {
   switch (action.type) {
     case 'GET_USERS':
-      return {
-        ...state,
-        users: loadingState,
-      };
     case 'GET_USERS_SUCCESS':
-      return {
-        ...state,
-        users: success(action.data),
-      };
     case 'GET_USERS_ERROR':
-      return {
-        ...state,
-        users: error(action.error),
-      };
+      return usersHandler(state, action);
     case 'GET_USER':
-      return {
-        ...state,
-        user: loadingState,
-      };
     case 'GET_USER_SUCCESS':
-      return {
-        ...state,
-        user: success(action.data),
-      };
     case 'GET_USER_ERROR':
-      return {
-        ...state,
-        user: error(action.error),
-      };
+      return userHandler(state, action);
     default:
       throw new Error(`Unhanded action type: ${action.type}`);
   }
