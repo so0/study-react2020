abc005a87551e1e253f32380b7c5fc65053d20f9
so0 <syngshm@gmail.com>
2020-08-30 19:27:48 +0900

    history 객체 사용

--- router-tutorial2/src/App.js
+++ router-tutorial2/src/App.js
@@ -5,6 +5,7 @@ import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Profiles from './Profiles';
+import HistorySample from './HistorySample';
 
 function App() {
   return (
@@ -19,11 +20,15 @@ function App() {
         <li>
           <Link to="/profiles">프로필 목록</Link>
         </li>
+        <li>
+          <Link to="/history">예제</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
       <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 }
