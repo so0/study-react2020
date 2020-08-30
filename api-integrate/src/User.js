6f8c9de6b926f706861a964c5d0a261e9cbf35bb
so0 <syngshm@gmail.com>
2020-08-30 17:59:38 +0900

    Context에서 사용하기

--- api-integrate/src/User.js
+++ api-integrate/src/User.js
@@ -1,23 +1,31 @@
 import React, { useState, useEffect, useReducer } from 'react';
 // import useAsync from './useAsync';
-import { useAsync } from 'react-async';
+// import { useAsync } from 'react-async';
+import { getUser, useUsersState, useUsersDispatch } from './UsersContext';
 import axios from 'axios';
 
-async function getUser({ id }) {
-  const response = await axios.get(
-    `https://jsonplaceholder.typicode.com/users/${id}`
-  );
-  return response.data;
-}
+// async function getUser({ id }) {
+//   const response = await axios.get(
+//     `https://jsonplaceholder.typicode.com/users/${id}`
+//   );
+//   return response.data;
+// }
 
 function User({ id }) {
-  const { data: user, error, isLoading } = useAsync({
-    promiseFn: getUser,
-    id,
-    watch: id,
-  });
+  // const { data: user, error, isLoading } = useAsync({
+  //   promiseFn: getUser,
+  //   id,
+  //   watch: id,
+  // });
   // const [state] = useAsync(() => getUser(id), [id]);
   // const { loading, data: user, error } = state;
+  const dispatch = useUsersDispatch();
+  const {
+    user: { loading: isLoading, data: user, error },
+  } = useUsersState();
+  useEffect(() => {
+    getUser(dispatch, id);
+  }, [dispatch, id]);
 
   if (isLoading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,28 +1,34 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useContext, useEffect, useReducer } from 'react';
 import axios from 'axios';
 // import useAsync from './useAsync';
 import { useAsync } from 'react-async';
 
 import User from './User';
-async function getUsers() {
-  const response = await axios.get(
-    `https://jsonplaceholder.typicode.com/users`
-  );
-  return response.data;
-}
+import { getUsers, useUsersState, useUsersDispatch } from './UsersContext';
+// async function getUsers() {
+//   const response = await axios.get(
+//     `https://jsonplaceholder.typicode.com/users`
+//   );
+//   return response.data;
+// }
 
 function Users({ id }) {
-  const { data: users, error, isLoading, run } = useAsync({
-    deferFn: getUsers,
-  });
+  // const { data: users, error, isLoading, run } = useAsync({
+  //   deferFn: getUsers,
+  // });
   // const [state, refetch] = useAsync(() => getUsers(id), [id]);
   const [userId, setUserId] = useState('');
 
+  const {
+    users: { loading: isLoading, data: users, error },
+  } = useUsersState();
+  const dispatch = useUsersDispatch();
   // const { loading, error, data: users } = state;
 
   if (isLoading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
-  if (!users) return <button onClick={run}>불러오기</button>;
+  if (!users)
+    return <button onClick={() => getUsers(dispatch)}>불러오기</button>;
   return (
     <>
       <ul>
@@ -35,7 +41,8 @@ function Users({ id }) {
           </li>
         ))}
       </ul>
-      fetchUsers <button onClick={run}>다시 불러오기</button>
+      fetchUsers{' '}
+      <button onClick={() => getUsers(dispatch)}>다시 불러오기</button>
       {userId && <User id={userId}></User>}
     </>
   );
