464318c4bcbf6396cb0f07525e9f665341e0e021
so0 <syngshm@gmail.com>
2020-08-30 19:41:53 +0900

    user-react-router 사용

--- router-tutorial2/src/Profiles.js
+++ router-tutorial2/src/Profiles.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, Route, NavLink } from 'react-router-dom';
 import Profile from './Profile';
 import WithRouterSample from './WithRouterSample';
+import RouterHookSample from './RouterHookSample';
 
 const Profiles = () => {
   return (
@@ -31,6 +32,7 @@ const Profiles = () => {
       />
       <Route path="/profiles/:username" component={Profile} />
       <WithRouterSample />
+      <RouterHookSample />
     </div>
   );
 };
